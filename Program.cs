b3941800d27f89e666dc1872747e8ad470d53060
3xp1o1t <jes.monti.503@gmail.com>
2024-03-31 22:27:52 -0600

    Add cache, swagger and custom cors

--- Program.cs
+++ Program.cs
@@ -1,7 +1,35 @@
+using Microsoft.AspNetCore.Cors;
 using MinimalAPICurso.Entidades;
 
 var builder = WebApplication.CreateBuilder(args);
+// Obtener un elemento de la configuracion
+var ambiente = builder.Configuration.GetValue<string>("Ambiente");
+var origenes = builder.Configuration.GetValue<string>("OrigenesPermitidos")!;
 // Inicia area de Servicios
+
+// CORS es un servicio
+builder.Services.AddCors(options =>
+{
+  // Politica por Defecto
+  options.AddDefaultPolicy(configuration =>
+  {
+    configuration.WithOrigins(origenes).AllowAnyHeader().AllowAnyMethod();
+  });
+
+  // Politica personalizada
+  options.AddPolicy("any", configuration =>
+  {
+    configuration.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+
+  });
+});
+
+// Servicio de cache
+builder.Services.AddOutputCache();
+
+// Habilitar servicio de swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Termina area de Servicios
 
@@ -9,13 +37,26 @@ var app = builder.Build();
 
 // Inicia area de Middlewares
 
-// Obtener un elemento de la configuracion
-var ambiente = builder.Configuration.GetValue<string>("ambiente");
+// Habilitar el middleware de Swagger antes de los cors
+// Es posible habilitar solo swagger cuando estemos desarrollando
+if (builder.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
 
-app.MapGet("/", () => "Ambiente actual: " + ambiente);
+// Habilitar el middleware de CORS
+app.UseCors();
 
-// Generos
-app.MapGet("/generos", () => {
+// Habilitar el middleware de cache
+app.UseOutputCache();
+
+// Este endpoint utiliza cualquier origen, cualquier cabecera y cualquier metodo
+app.MapGet("/", [EnableCors(policyName: "any")] () => "Ambiente actual: " + ambiente);
+
+// Generos con cache habilitado
+app.MapGet("/generos", () =>
+{
 
   var generos = new List<Genero>
   {
@@ -25,7 +66,7 @@ app.MapGet("/generos", () => {
   };
 
   return generos;
-});
+}).CacheOutput(c => c.Expire(TimeSpan.FromSeconds(60)));
 
 // Termina area de Middlewares
 
--- README.md
+++ README.md
@@ -16,9 +16,32 @@ dotnet new web -o MinimalAPICurso
 
 ## Conceptos fundamentales de Net
 
-1. **Solucion** es un conjunto de proyectos
+1. **Solucion** e ws un conjunto de proyectos
 2. **csproj** es un archivo similar a package.json
 3. **launchSettings.json** es un archivo que solo es util en modo dev
 4. **appsettings.json** este en conjunto con **development** son archivos donde se almacena configuracion la cual puede ser accedida desde nuestro proyecto, todo depende del entorno de trabajo (desa/prod)
 5. **Servicios y middleware** una buena practica es acomodar correctamente nuestros servicios y middleware en appsettings
 6. **Perdonar nulos** - Para perdonar un valor null cuando se declara en una variable y omitir la configuracion (<Nullable>enable</Nullable>) es posible usar **!** al final de null (string name {get;set;} = null!).
+7. CORS es un Servicio, para permitir que todo se ejecute se puede hacer lo siguiente
+
+   ```csharp
+    // Program.cs
+    builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(configuration =>
+    {
+      configuration.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    }));
+   ```
+
+8. **WithOrigins** permite definir una lista de origenes permitidos
+
+```json
+  "OrigenesPermitidos": "*",
+```
+
+9. **AddPolicy** se puede tener muchas politicas diferentes.
+10. Es posible utilizar cualquier politica directamente en un endpoint
+
+11. **Cache** Configurar el servicio de cache ayuda a mejorar el rendimiento de la aplicacion.
+
+12. **MIDDLEWARE ORDEN** el orden de los middleware es importante, pues determina como seran ejecutados
